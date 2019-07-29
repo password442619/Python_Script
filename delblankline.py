3bd80ce81b69fbf4bc7d5355822aa22afb15127c
password442619 <42017683+password442619@users.noreply.github.com>
2019-07-29 12:01:45 +0800

    Create delblankline.py

---
+++ delblankline.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+def delblankline(infile, outfile):
+	infopen = open(infile,'r', encoding='utf-8')
+	outfopen = open(outfile, 'w', encoding='utf-8')
+	lines = infopen.readlines()
+	for line in lines:
+		if line.split():
+			outfopen.writelines(line)
+		else:
+			outfopen.writelines("")
+	infopen.close()
+	outfopen.close()
+
+delblankline("test.txt", "new.txt")
