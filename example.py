3af83064c5391596d5c4631949904c9ce8d53add
password442619 <42017683+password442619@users.noreply.github.com>
2020-03-06 09:50:12 +0800

    提取文件名中的汉字和数字

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+########示例1#########
 # -*- coding: utf-8 -*-
 import re
 content = """
@@ -15,3 +16,36 @@ def translate(str):
 	return outStr
 content_format = content.decode('utf-8')
 print(translate(content_format))
+########示例2########
+# -*- coding: utf-8 -*-
+import os 
+import sys
+import argparse
+import re
+
+def translate(str):
+	line = str.strip()
+	pattern = re.compile('[\u4e00-\u9fa5]')
+	zh = "".join(pattern.split(line)).strip()
+	outStr = zh
+	return outStr
+
+def main(args):
+    for filename in os.listdir(args.path):
+		name_tmp = filename.split('.')
+		name = name_tmp[0]
+		stu_num_tmp = name.split('_')
+		stu_num = stu_num_tmp[1]
+		stu_name = translate(name)
+		print(stu_name)
+		#print(stu_num)
+def parser_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path',dest='path',type=str,default='/Users/pangweidong/Desktop/test',help='默认目录/Users/pangweidong/Desktop/test')
+    parser.add_argument('--num',dest='num',type=int,default=100,help='默认输出个数')
+    return parser.parse_args(argv)
+
+
+
+if __name__ == '__main__':
+    main(parser_arguments(sys.argv[1:]))
