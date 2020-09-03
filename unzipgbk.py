3e73e5a56fbbc4eb4eab72d56322e446a5d16aed
password442619 <42017683+password442619@users.noreply.github.com>
2020-09-03 11:29:39 +0800

    解决UnicodeError问题。

--- unzipgbk.py
+++ unzipgbk.py
@@ -11,7 +11,11 @@ pathname = os.path.dirname(os.path.abspath(sys.argv[1]))
 
 file = zipfile.ZipFile(sys.argv[1], "r")
 for name in file.namelist():
-  utf8name = name.decode('gbk')
+  try:
+    utf8name = name.decode('gbk', 'ignore')
+  except UnicodeError as u:
+    continue
   print "Extracting" + uft8name
   file.extract(utf8name, pathname)
   #解压zip文件，使用方法./unzipgbk.py XXX.zip
+  #此脚本用于解决ubuntu系统解压windows上传的zip包，解压出的文件中文乱码问题。
